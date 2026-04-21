055d3145cf833ab7cb73aa0ae4a513add3836271
parmargunjan33@gmail.com <parmargunjan33@gmail.com>
2026-04-21 16:59:00 +0530

    additional features

--- database/migrations/2026_04_20_000002_add_email_to_otps_table.php
+++ database/migrations/2026_04_20_000002_add_email_to_otps_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::table('otps', function (Blueprint $table) {
             $table->string('email', 255)->nullable()->after('mobile');
             $table->dropIndex(['mobile']);
-            $table->index(['email', 'mobile']);
         });
     }
 
